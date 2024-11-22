5d6fea7c4442ddb790ec31a6182c26ddf0f93b91
Debbie Stubbs <debbie.coates@hotmail.co.uk>
2024-11-22 11:04:56 +0000

    Set up structure of fame functions

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+function runGame(){
+}
+
+function checkAnswer(){
+}   
+
+function calculateCorrectAnswer(){
+}   
+
+function incrementScore(){
+}       
+
+function incrementwrongAnswr(){
+}
+
+function displayAdditionQuestion(){
+}       
+
+function displaysubtractQuestion(){
+}   
+
+function displaymultiplyQuestion(){
+}   
